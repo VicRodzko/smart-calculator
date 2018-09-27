172f83f0b3edb2d9be7fa133dfceba2e92efe3da
Victor Rodzko <vicrodzko@mail.ru>
2018-09-27 08:23:58 +0300

    Calculator

--- src/index.js
+++ src/index.js
@@ -1,26 +1,69 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.result = [].concat([initialValue]);
   }
 
   add(number) {
-    // your implementation
+    this.result.push("+", number);
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this.result.push("-", number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.result.push("*", number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.result.push("/", number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.result.push("^", number);
+    return this;
+  }
+
+  valueOf() {
+
+    var arr = this.result;
+    var arrLength = this.result.length;
+    
+    for(var i = arrLength; i >= 0; i--) {
+      if(arr[i] == "^") {
+        arr.splice(i - 1, 3, Math.pow(arr[i - 1], arr[i + 1]));
+        i = arrLength;
+      }
+      arrLength = arr.length;
+    }
+
+    for(var i = arrLength; i >= 0; i--) {
+      if(arr[i] == "*") {
+        arr.splice(i - 1, 3, arr[i - 1] * arr[i + 1]);
+        i = arrLength;
+      } else if (arr[i] == "/") {
+        arr.splice(i - 1, 3, arr[i - 1] / arr[i + 1]);
+        i = arrLength;
+      }
+      arrLength = arr.length;
+    }
+    
+    for(var i = 0; i < arrLength; i++) {
+      if(arr[i] == "+") {
+        arr.splice(i - 1, 3, +arr[i - 1] + +arr[i + 1]);
+        i = 0;
+      } else if (arr[i] == "-"){
+        arr.splice(i - 1, 3, arr[i - 1] - arr[i + 1]);
+        i = 0;
+      }
+      arrLength = arr.length;
+    }
+
+    return arr[0];
   }
 }
 
